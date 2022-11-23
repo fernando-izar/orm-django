dfebc7bb5f7db1135b24dfc7608833cbaf5bb1e0
fernando-izar <fizar@me.com>
2022-11-23 15:52:53 -0300

    feat: Aula Views

--- musicians/views.py
+++ musicians/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from musicians.models import Musician
+from django.forms.models import model_to_dict
 
-# Create your views here.
+
+class MusicianView(APIView):
+    def post(self, request):
+        musician_data = request.data
+
+        musician = Musician.objects.create(**musician_data)
+
+        # musician = Musician.objects.create(
+        #     first_name=musician_data["first_name"],
+        #     last_name=musician_data["last_name"],
+        #     instrument=musician_data["instruments"],
+        # )
+
+        return Response(model_to_dict(musician), 201)
+
+    def get(self, request):
+        musicians = Musician.objects.all()
+
+        musicians_dict = musicians.values()
+
+        # musicians = musicians.values()
+        # musicians_dict = []
+
+        # for musician in musicians:
+        #     m = model_to_dict(musician)
+        #     musicians_dict.append(m)
+
+        return Response(musicians_dict)
